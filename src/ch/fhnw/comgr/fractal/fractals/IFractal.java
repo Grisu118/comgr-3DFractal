f870fd1b0946f8810d119c91a1419e057d03ddcb
Benjamin Leber <grisu118@grisu118.ch>
2015-10-30 10:44:58 +0100

    JavaDoc

--- src/ch/fhnw/comgr/fractal/fractals/IFractal.java
+++ src/ch/fhnw/comgr/fractal/fractals/IFractal.java
@@ -9,10 +9,20 @@ import java.util.List;
  */
 public interface IFractal {
 
+    /**
+     * Method for init the fractal data, add to scene.
+     */
     void init();
 
+    /**
+     * List with all widgets which are needed for the fractal.
+     * @return A list containing all needed widgets.
+     */
     List<IWidget> getWidgets();
 
+    /**
+     * Cleanup Memory etc.
+     */
     void cleanup();
 
 }
