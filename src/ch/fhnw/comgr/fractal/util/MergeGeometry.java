c66a60d36d2cdf50379491e5e664ac5891b97f30
Benjamin Leber <grisu118@grisu118.ch>
2015-10-30 11:50:40 +0100

    Fix for ether-gl changes

--- src/ch/fhnw/comgr/fractal/util/MergeGeometry.java
+++ src/ch/fhnw/comgr/fractal/util/MergeGeometry.java
@@ -13,11 +13,13 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 /**
  * Created by benjamin on 29.10.2015.
  */
-public class MergeGeometry extends DefaultGeometry {
+public class MergeGeometry extends AbstractGeometry {
 
-    private Primitive type;
-    private IGeometryAttribute[] attributes;
-    private float[][] data;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    private final Primitive type;
+    private final IGeometryAttribute[] attributes;
+    private final float[][] data;
     private ArrayList<float[][]> dataList;
 
     /**
@@ -30,12 +32,117 @@ public class MergeGeometry extends DefaultGeometry {
      * @param data
      */
     public MergeGeometry(Primitive type, IGeometryAttribute[] attributes, float[][] data) {
-        super(type, attributes, data);
+        super(type);
+
+
+        if (attributes[0] != POSITION_ARRAY)
+            throw new IllegalArgumentException("first attribute must be position");
+        if (attributes.length != data.length)
+            throw new IllegalArgumentException("# attribute type != # attribute data");
+        checkAttributeConsistency(attributes, data);
         this.type = type;
-        this.attributes = attributes;
+        this.attributes = Arrays.copyOf(attributes, attributes.length);
+        this.data = new float[data.length][];
+        for (int i = 0; i < data.length; ++i) {
+            this.data[i] = Arrays.copyOf(data[i], data[i].length);
+        }
         this.dataList = new ArrayList<>();
         this.dataList.add(data);
-        this.data = data;
+    }
+
+
+    private MergeGeometry(MergeGeometry g) {
+        super(g.getType());
+        this.type = g.type;
+        attributes = Arrays.copyOf(g.attributes, g.attributes.length);
+        this.data = new float[g.data.length][];
+        for (int i = 0; i < g.data.length; ++i) {
+            this.data[i] = Arrays.copyOf(g.data[i], g.data[i].length);
+        }
+        this.dataList = (ArrayList<float[][]>) g.dataList.clone();
+    }
+
+    /**
+     * Create copy of this geometry.
+     *
+     * @return the copy
+     */
+    public MergeGeometry copy() {
+        try {
+            lock.readLock().lock();
+            return new MergeGeometry(this);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public IGeometryAttribute[] getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public float[][] getData() {
+        return data;
+    }
+
+    @Override
+    public void inspect(int index, IAttributeVisitor visitor) {
+        try {
+            lock.readLock().lock();
+            visitor.visit(attributes[index], data[index]);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public void inspect(IAttributesVisitor visitor) {
+        try {
+            lock.readLock().lock();
+            visitor.visit(attributes, data);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public void modify(int index, IAttributeVisitor visitor) {
+        try {
+            lock.writeLock().lock();
+            visitor.visit(attributes[index], data[index]);
+        } finally {
+            lock.writeLock().unlock();
+        }
+        updateRequest();
+    }
+
+    @Override
+    public void modify(IAttributesVisitor visitor) {
+        try {
+            lock.writeLock().lock();
+            visitor.visit(attributes, data);
+            checkAttributeConsistency(attributes, data);
+        } finally {
+            lock.writeLock().unlock();
+        }
+        updateRequest();
+    }
+
+    private static void checkAttributeConsistency(IGeometryAttribute[] attributes, float[][] data) {
+        // check for correct individual lengths
+        for (int i = 0; i < attributes.length; ++i) {
+            if (data[i].length % attributes[i].getNumComponents() != 0)
+                throw new IllegalArgumentException(attributes[i].id() + ": size " + data[i].length + " is not a multiple of attribute size " + attributes[i].getNumComponents());
+        }
+
+        // check for correct overall lengths
+        int numElements = data[0].length / attributes[0].getNumComponents();
+        for (int i = 1; i < attributes.length; ++i) {
+            int ne = data[i].length / attributes[i].getNumComponents();
+            if (ne != numElements)
+                throw new IllegalArgumentException(attributes[i].id() + ": size " + ne + " does not match size of position attribute (" + numElements + ")");
+        }
     }
 
     public void merge(MergeGeometry g) {
