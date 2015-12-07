d25f614cdc23480a9dbd2c7dbaf71295a3620307
Benjamin Leber <grisu118@grisu118.ch>
2015-12-07 09:26:43 +0100

    Added Fog

--- src/ch/fhnw/comgr/fractal/fractals/mandel/glsl/mandelbulb_vert.glsl
+++ src/ch/fhnw/comgr/fractal/fractals/mandel/glsl/mandelbulb_vert.glsl
@@ -13,10 +13,20 @@ in vec4 vertexColor;
 out vec4 vsColor;
 
 void main() {
+    vec4 vertex = view.viewProjMatrix * vertexPosition;
 	vsColor = materialColor;
-	if (useVertexColors)
+	if (useVertexColors) {
 		vsColor *= vertexColor;
+	}
+	vec4 whiteColor = vec4(1,1,1,1);
+    float dFog = 0.3;
 
-	gl_PointSize = vertexPointSize;
-	gl_Position = view.viewProjMatrix * vertexPosition;
+    vec4 fogColor = vec4(0.1f, 0.2f, 0.3f, 1.0f);
+    float x = dFog * vertex.z;
+    float s = exp(-x*x);
+    vec4 interplatedColor = s*vsColor + (1-s)*fogColor;
+    vsColor.rgb = min(interplatedColor.rgb, whiteColor.rgb);
+
+	gl_PointSize = 3; //TODO form java code
+	gl_Position = vertex;
 }
