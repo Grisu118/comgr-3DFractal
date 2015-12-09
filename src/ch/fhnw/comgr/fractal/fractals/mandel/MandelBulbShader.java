2171a6accc23f88ed94d9a0c80c5010d509a7d12
Grisu118 <grisu118@grisu118.ch>
2015-12-09 16:17:19 +0100

    colorized ;)

--- src/ch/fhnw/comgr/fractal/fractals/mandel/MandelBulbShader.java
+++ src/ch/fhnw/comgr/fractal/fractals/mandel/MandelBulbShader.java
@@ -31,11 +31,12 @@ public class MandelBulbShader extends AbstractShader {
         if (useVertexPointSize)
             addArray(new PointSizeArray());
 
-        addUniform(new BooleanUniform("shader.vertex_colors_flag", "useVertexColors", () -> useVertexColors));
+        addUniform(new BooleanUniform("shader.vertex_colors_flag", "useVertexColors", () -> false));
         addUniform(new BooleanUniform("shader.texture_flag", "useTexture", () -> false));
 
         addUniform(new ColorUniform(attributes.contains(IMaterial.COLOR) ? null : () -> RGBA.WHITE));
         addUniform(new FloatUniform(IMaterial.POINT_SIZE, "pointSize", attributes.contains(IMaterial.POINT_SIZE) ? null : () -> 3f));
+        addUniform(new FloatUniform("custom.maxDistance", "maxDistance", () -> 0.4f));
 
         addUniform(new StateInject("shader.point_size_program", (gl, p) -> gl.glEnable(GL3.GL_PROGRAM_POINT_SIZE),
                 (gl, p) -> gl.glDisable(GL3.GL_PROGRAM_POINT_SIZE)));
--- src/ch/fhnw/comgr/fractal/fractals/mandel/glsl/mandelbulb_vert.glsl
+++ src/ch/fhnw/comgr/fractal/fractals/mandel/glsl/mandelbulb_vert.glsl
@@ -6,17 +6,56 @@ uniform bool useVertexColors;
 
 uniform vec4 materialColor;
 uniform float vertexPointSize;
+uniform float maxDistance;
 
 in vec4 vertexPosition;
 in vec4 vertexColor;
 
 out vec4 vsColor;
 
+/*
+ * GLSL HSV to RGB+A conversion. Useful for many effects and shader debugging.
+ *
+ * Copyright (c) 2012 Corey Tabaka
+ *
+ * Hue is in the range [0.0, 1.0] instead of degrees or radians.
+ * Alpha is simply passed through for convenience.
+ */
+
+vec4 hsv_to_rgb(float h, float s, float v, float a)
+{
+	float c = v * s;
+	h = mod((h * 6.0), 6.0);
+	float x = c * (1.0 - abs(mod(h, 2.0) - 1.0));
+	vec4 color;
+
+	if (0.0 <= h && h < 1.0) {
+		color = vec4(c, x, 0.0, a);
+	} else if (1.0 <= h && h < 2.0) {
+		color = vec4(x, c, 0.0, a);
+	} else if (2.0 <= h && h < 3.0) {
+		color = vec4(0.0, c, x, a);
+	} else if (3.0 <= h && h < 4.0) {
+		color = vec4(0.0, x, c, a);
+	} else if (4.0 <= h && h < 5.0) {
+		color = vec4(x, 0.0, c, a);
+	} else if (5.0 <= h && h < 6.0) {
+		color = vec4(c, 0.0, x, a);
+	} else {
+		color = vec4(0.0, 0.0, 0.0, a);
+	}
+
+	color.rgb += v - c;
+
+	return color;
+}
+
 void main() {
     vec4 vertex = view.viewProjMatrix * vertexPosition;
 	vsColor = materialColor;
 	if (useVertexColors) {
-		vsColor *= vertexColor;
+	    float dist = length(vertexPosition);
+		vsColor *= hsv_to_rgb(dist/maxDistance, 1, 1, 1);
 	}
 	vec4 whiteColor = vec4(1,1,1,1);
     float dFog = 0.3;
@@ -30,3 +69,4 @@ void main() {
 	gl_PointSize = 3; //TODO form java code
 	gl_Position = vertex;
 }
+
